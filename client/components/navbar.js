90b72fa7bd23e9a2477f57328ae8239321011c9a
Vanessa Chu <vchu22@users.noreply.github.com>
2019-03-08 12:36:37 -0500

    replaced boilerplate

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -6,7 +6,7 @@ import {logout} from '../store'
 
 const Navbar = ({handleClick, isLoggedIn}) => (
   <div>
-    <h1>BOILERMAKER</h1>
+    <h1>1901-gh-stackathon</h1>
     <nav>
       {isLoggedIn ? (
         <div>
