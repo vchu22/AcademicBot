206d207b5a51ed9b43c191b5854c84df1d971fcd
Vanessa Chu <vanessa.chu@outlook.com>
2019-03-10 15:27:04 -0400

    changed favicon and added webhook response to Dialogflow

--- server/webhook.js
+++ server/webhook.js
@@ -1,6 +1,29 @@
 const router = require('express').Router()
+const axios = require('axios')
 module.exports = router
 
 router.post('/', (req, res, next) => {
-  console.log(req.body)
+  try {
+    const intent = req.body.queryResult.intent.displayName
+    const params = req.body.queryResult.parameters
+    console.log({intent, params})
+    const response = createResponse()
+    console.log(response)
+    res.json(response)
+  } catch (err) {
+    res.status(404).send('Not Found')
+  }
 })
+
+function createResponse() {
+  return {
+    fulfillmentText: 'This is just a response',
+    fulfillmentMessages: [
+      {
+        text: {
+          text: ['This is just a response']
+        }
+      }
+    ]
+  }
+}
