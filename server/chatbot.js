ccc92d938eaef53659ab048ef56f85b347ab9dc7
Vanessa Chu <vanessa.chu@outlook.com>
2019-03-10 21:08:18 -0400

    used switch case statement for createResponse

--- server/chatbot.js
+++ server/chatbot.js
@@ -15,40 +15,42 @@ router.post('/webhook', async (req, res, next) => {
 })
 
 async function createResponse(intent, params) {
-  if (intent === 'Default Welcome Intent') {
-    return {
-      fulfillmentText:
-        'Welcome to the Academic Advising Center! How can I help you?',
-      fulfillmentMessages: [
-        {
-          text: {
-            text: [
-              'Welcome to the Academic Advising Center! How can I help you?'
-            ]
-          }
-        }
-      ]
-    }
-  } else if (intent === 'List Available Advisers') {
-    const response = await Advisor.findAll({
-      where: {
-        available: true
+  let returnedObj = {
+    fulfillmentText: '',
+    fulfillmentMessages: [
+      {
+        text: {text: ['']}
       }
-    })
-    const advisors = response.map(
-      item => `${item.dataValues.firstname} ${item.dataValues.lastname} `
-    )
-    const insertText = advisors.join(', ').replace(/, ([^,]*)$/, ' and $1')
-    const text = `Sure. We have ${insertText} available. Do you want to make an appointment with them?`
-    return {
-      fulfillmentText: text,
-      fulfillmentMessages: [
-        {
-          text: {
-            text: [text]
-          }
-        }
-      ]
-    }
+    ]
   }
+  switch (intent) {
+    case 'Default Welcome Intent':
+      returnedObj.fulfillmentText = returnedObj.fulfillmentMessages[0].text.text[0] =
+        'Welcome to the Academic Advising Center! How can I help you?'
+      return returnedObj
+    case 'List Available Advisers':
+      const response = await Advisor.findAll({
+        where: {
+          available: true
+        }
+      })
+      const advisors = response.map(
+        item => `${item.dataValues.firstname} ${item.dataValues.lastname}`
+      )
+      const insertText = advisors.join(', ').replace(/, ([^,]*)$/, ' and $1')
+      const text = [
+        `Sure. We have ${insertText} available. Do you want to make an appointment with them?`,
+        `Sure. ${insertText} are available right now. Do you want to make an appointment with them?`
+      ]
+      returnedObj.fulfillmentText = returnedObj.fulfillmentMessages[0].text.text[0] = randomChoice(
+        text
+      )
+      return returnedObj
+    default:
+      return returnedObj
+  }
+}
+
+function randomChoice(arr) {
+  return arr[Math.floor(Math.random() * arr.length)]
 }
