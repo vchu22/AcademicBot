31a093a5b2ad766c93cd244647a8b017eaebae78
Vanessa Chu <vchu22@users.noreply.github.com>
2019-05-18 20:30:53 -0400

    upgraded sequelize ver & changed db connection str

---
+++ client/secret.js
@@ -0,0 +1,2 @@
+var link = ''
+export {link}
--- server/db/db.js
+++ server/db/db.js
@@ -2,9 +2,14 @@ const Sequelize = require('sequelize')
 const pkg = require('../../package.json')
 
 const databaseName = pkg.name + (process.env.NODE_ENV === 'test' ? '-test' : '')
+const dbUsername = process.env.dbUsername
+const dbPassword = process.env.dbPassword
 
 const db = new Sequelize(
-  process.env.DATABASE_URL || `postgres://localhost:5432/${databaseName}`,
+  process.env.DATABASE_URL ||
+    `postgres://${
+      dbUsername ? `${dbUsername}:${dbPassword}@` : ''
+    }localhost:5432/${databaseName}`,
   {
     logging: false
   }
